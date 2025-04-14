16dea0250f9f98af45c00f458652a7220a20a15c
Arielchae <cothgud@gmail.com>
2025-04-14 09:50:45 +0900

    [ADD] store 지우고 mapper 인터페이스 위치 변경
    공인중개사, 일반회원 css 수정

--- JachuiHouse/src/main/java/com/house/jachui/member/mapper/MemberMapper.java
+++ JachuiHouse/src/main/java/com/house/jachui/member/mapper/MemberMapper.java
@@ -1,12 +1,16 @@
-package com.house.jachui.model.dao.MemberMapper;
+package com.house.jachui.member.mapper;
 
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSession;
 
 import com.house.jachui.member.vo.MemberVO;
 
 @Mapper
 public interface MemberMapper {
 	List<MemberVO> findIds(MemberVO member);
+	
+	int memberSignupJachui(MemberVO member);
+
 }
