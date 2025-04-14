9dbcd4dcbc290692cf5c87b3adc51ee7243a5d9a
Arielchae <cothgud@gmail.com>
2025-04-14 19:35:40 +0900

    [ADD] 로그인 로고이미지 삽입, 로고와 텍스트 정렬 완료.

--- JachuiHouse/src/main/java/com/house/jachui/member/vo/MemberVO.java
+++ JachuiHouse/src/main/java/com/house/jachui/member/vo/MemberVO.java
@@ -1,7 +1,19 @@
 package com.house.jachui.member.vo;
 
 import java.sql.Date;
+import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberVO {
 	private String userId;
 	private String userPw;
@@ -21,156 +33,4 @@ public class MemberVO {
 	private String approveYn;  // 이건 내가 넣어야하는건지 물어보기
 	private Date joinDate;		//추가 erd에도 추가하기.
 
-	public MemberVO() {}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getUserPw() {
-		return userPw;
-	}
-
-	public void setUserPw(String userPw) {
-		this.userPw = userPw;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getUserEmail() {
-		return userEmail;
-	}
-
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-
-	public String getUserPhone() {
-		return userPhone;
-	}
-
-	public void setUserPhone(String userPhone) {
-		this.userPhone = userPhone;
-	}
-
-	public String getUserGender() {
-		return userGender;
-	}
-
-	public void setUserGender(String userGender) {
-		this.userGender = userGender;
-	}
-
-	public String getUserAddress() {
-		return userAddress;
-	}
-
-	public void setUserAddress(String userAddress) {
-		this.userAddress = userAddress;
-	}
-
-	public Date getUserBirth() {
-		return userBirth;
-	}
-
-	public void setUserBirth(Date userBirth) {
-		this.userBirth = userBirth;
-	}
-
-	public String getUserPostcode() {
-		return userPostcode;
-	}
-
-	public void setUserPostcode(String userPostcode) {
-		this.userPostcode = userPostcode;
-	}
-
-	public String getUserDetailAddress() {
-		return userDetailAddress;
-	}
-
-	public void setUserDetailAddress(String userDetailAddress) {
-		this.userDetailAddress = userDetailAddress;
-	}
-
-	public String getAloneLiving() {
-		return aloneLiving;
-	}
-
-	public void setAloneLiving(String aloneLiving) {
-		this.aloneLiving = aloneLiving;
-	}
-
-	public String getUserRole() {
-		return userRole;
-	}
-
-	public void setUserRole(String userRole) {
-		this.userRole = userRole;
-	}
-
-	public String getRealtorName() {
-		return realtorName;
-	}
-
-	public void setRealtorName(String realtorName) {
-		this.realtorName = realtorName;
-	}
-
-	public String getRealtorNo() {
-		return realtorNo;
-	}
-
-	public void setRealtorNo(String realtorNo) {
-		this.realtorNo = realtorNo;
-	}
-
-	public String getDeleteYn() {
-		return deleteYn;
-	}
-
-	public void setDeleteYn(String deleteYn) {
-		this.deleteYn = deleteYn;
-	}
-
-	public String getApproveYn() {
-		return approveYn;
-	}
-
-	public void setApproveYn(String approveYn) {
-		this.approveYn = approveYn;
-	}
-
-	public Date getJoinDate() {
-		return joinDate;
-	}
-
-	public void setJoinDate(Date joinDate) {
-		this.joinDate = joinDate;
-	}
-
-	@Override
-	public String toString() {
-		return "MemberVO [userId=" + userId + ", userPw=" + userPw + ", userName=" + userName + ", userEmail="
-				+ userEmail + ", userPhone=" + userPhone + ", userGender=" + userGender + ", userAddress=" + userAddress
-				+ ", userBirth=" + userBirth + ", userPostcode=" + userPostcode + ", userDetailAddress="
-				+ userDetailAddress + ", aloneLiving=" + aloneLiving + ", userRole="
-				+ userRole + ", realtorName=" + realtorName + ", realtorNo=" + realtorNo + ", deleteYn=" + deleteYn
-				+ ", approveYn=" + approveYn + ", joinDate=" + joinDate + "]";
-	}
-	
-	
 }
-
-
-
