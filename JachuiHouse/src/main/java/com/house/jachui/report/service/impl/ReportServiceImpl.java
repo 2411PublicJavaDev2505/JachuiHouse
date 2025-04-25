623a454716bdafc9fb489aab05cd564f2e66b1f3
Arielchae <cothgud@gmail.com>
2025-04-25 14:02:44 +0900

    [ADD] 아이디 찾기에서 alert 예쁜 모달창으로 구현완료

--- JachuiHouse/src/main/java/com/house/jachui/report/service/impl/ReportServiceImpl.java
+++ JachuiHouse/src/main/java/com/house/jachui/report/service/impl/ReportServiceImpl.java
@@ -1,11 +1,13 @@
 package com.house.jachui.report.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.house.jachui.report.controller.dto.ReportInsertRequest;
 import com.house.jachui.report.mapper.ReportMapper;
 import com.house.jachui.report.service.ReportService;
 
+@Service
 public class ReportServiceImpl implements ReportService{
 	
 	private ReportMapper rMapper;
