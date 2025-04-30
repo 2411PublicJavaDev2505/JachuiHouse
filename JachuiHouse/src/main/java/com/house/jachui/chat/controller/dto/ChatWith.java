aee90d3f3f17cc91c0779d989b5210095aea3bb5
felixhsm <felixhsm9410@gmail.com>
2025-04-30 15:03:10 +0900

    ADD ChatWith 빠져서 올림

---
+++ JachuiHouse/src/main/java/com/house/jachui/chat/controller/dto/ChatWith.java
@@ -0,0 +1,20 @@
+package com.house.jachui.chat.controller.dto;
+
+import com.house.jachui.chat.model.vo.Chat;
+import com.house.jachui.chat.model.vo.ChatRoom;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatWith {
+	private Chat chat;
+	private ChatRoom chatRoom;
+	private String opponentId;
+	}
